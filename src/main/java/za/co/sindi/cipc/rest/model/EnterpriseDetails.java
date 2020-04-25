f5bbc2415da76bb9acda3b2f030b229d55efc022
Buhake Sindi <buhake.sindi@sindi.co.za>
2020-04-26 01:47:29 +0200

    Fixed unwanted imports.

--- src/main/java/za/co/sindi/cipc/rest/model/EnterpriseDetails.java
+++ src/main/java/za/co/sindi/cipc/rest/model/EnterpriseDetails.java
@@ -8,7 +8,6 @@ import java.util.Date;
 
 import javax.json.bind.annotation.JsonbDateFormat;
 import javax.json.bind.annotation.JsonbProperty;
-import javax.json.bind.annotation.JsonbTypeAdapter;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
